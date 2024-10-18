912c0c2caa9a522382d2f4476984ecf950db3f8e
Leonardo-snts <leonardosantos.ms77@gmail.com>
2024-10-18 09:16:03 -0400

    adicionando empresas e removendo endpoints desncessarios

--- src/components/PriceComparison.tsx
+++ src/components/PriceComparison.tsx
@@ -14,6 +14,13 @@ const PriceComparison = () => {
   const amazonMRP = data.map(item => item['Amazon MRP']);
   const flipkartMRP = data.map(item => item['Flipkart MRP']);
   const myntraMRP = data.map(item => item['Myntra MRP']);
+  const amazonFbaMrp = data.map(item => item['Amazon FBA MRP']);
+  const aijoMRP = data.map(item => item['Aijo MRP']);
+  const limeroadMRP = data.map(item => item['Limeroad MRP']);
+  const paytmMRP = data.map(item => item['Paytm MRP']);
+  const snapdealMRP = data.map(item => item['Snadeal MRP']);
+
+
 
   return (
     <Plot
@@ -36,8 +43,38 @@ const PriceComparison = () => {
           name: 'Myntra MRP',
           type: 'bar',
         },
+        {
+          x: sku,
+          y: amazonFbaMrp,
+          name: 'Amazon FBA MRP',
+          type: 'bar',
+        },
+        {
+          x: sku,
+          y: aijoMRP,
+          name: 'Myntra MRP',
+          type: 'bar',
+        },
+        {
+          x: sku,
+          y: limeroadMRP,
+          name: 'Limeroad MRP',
+          type: 'bar',
+        },
+        {
+          x: sku,
+          y: paytmMRP,
+          name: 'Paytm MRP',
+          type: 'bar',
+        },
+        {
+          x: sku,
+          y: snapdealMRP,
+          name: 'Snapdeal MRP',
+          type: 'bar',
+        },
       ]}
-      layout={{ title: 'Comparação de Preços MRP (Amazon, Flipkart, Myntra)', barmode: 'group' }}
+      layout={{ title: 'Comparação de Preços', barmode: 'group' }}
     />
   );
 };
